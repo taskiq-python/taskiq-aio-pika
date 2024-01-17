97b802b5677624be4921f2cdeca7068ef837d83e
Dmitriy <dimastbk@proton.me>
2024-01-17 21:32:29 +0600

    chore: remove unused fallback for python 3.7

--- taskiq_aio_pika/__init__.py
+++ taskiq_aio_pika/__init__.py
@@ -1,12 +1,7 @@
 """Taskiq integration with aio-pika."""
-from taskiq_aio_pika.broker import AioPikaBroker
+from importlib.metadata import version
 
-try:
-    # Python 3.8+
-    from importlib.metadata import version  # noqa: WPS433
-except ImportError:
-    # Python 3.7
-    from importlib_metadata import version  # noqa: WPS433, WPS440
+from taskiq_aio_pika.broker import AioPikaBroker
 
 __version__ = version("taskiq-aio-pika")
 
